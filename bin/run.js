db0106036e72fbe6e47fd25b67ece256cb819465
faizanq <faizanqureshi0@gmail.com>
2018-09-26 21:49:43 +0530

    server start listening in development

---
+++ bin/run.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const service = require('../server/service');
+
+const http = require('http');
+
+const server = http.createServer(service);
+
+server.listen(3000);
+
+server.on('listening',()=>console.log(
+	`Server is listening on ${server.address().port} and running in ${service.get('env')} mode.}`
+	));
+
